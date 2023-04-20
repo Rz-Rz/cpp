9a2bc6732d1a3e7a4bd9cc1901125974c8826191
Karim Dhrif <karimdhrif4444@gmail.com>
2023-04-20 15:51:28 +0200

    ex03 done

---
+++ cpp01/ex03/HumanB.hpp
@@ -0,0 +1,18 @@
+#ifndef HUMANB_HPP
+#define HUMANB_HPP
+
+#include <string>
+#include "Weapon.hpp"
+
+class HumanB {
+public:
+    HumanB(const std::string& name);
+    void setWeapon(Weapon& weapon);
+    void attack() const;
+
+private:
+    std::string _name;
+    Weapon* _weapon;
+};
+
+#endif // HUMANB_HPP
