3f79229893793ef7557346f7056242e9abd04911
Karim DHRIF <kdhrif@paul-f4Br6s1.clusters.42paris.fr>
2023-07-18 16:56:07 +0200

    cpp08 ex02 done

--- cpp08/ex01/span.hpp
+++ cpp08/ex01/span.hpp
@@ -1,1 +1,26 @@
 #include <vector>
+#include <algorithm>
+#include <climits>
+#include <stdexcept>
+
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+class Span
+{ 
+  private:
+    unsigned int _N;
+    std::vector<int> _vec;
+
+  public:
+    Span();
+    Span(unsigned int N);
+    Span(const Span& other);
+    ~Span();
+    Span& operator=(const Span& other);
+
+    void addNumber(int number);
+    int shortestSpan();
+    int longestSpan();
+};
+#endif
