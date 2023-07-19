28da20bd3bf68deb17c7883b67577fe1d66efc8e
Karim DHRIF <kdhrif@paul-f4Br8s2.clusters.42paris.fr>
2023-07-19 17:15:31 +0200

    finished ex02 cpp08

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -1,8 +1,9 @@
 #include <stack>
 #include <deque>
+#include <stdexcept>
 
 template <typename T>
-class MutantStack : public std::stack<T, std::deque<T>>
+class MutantStack : public std::stack<T, std::deque<T> >
 {
 public:
     typedef typename std::deque<T>::iterator iterator;
@@ -12,4 +13,17 @@ public:
     const_iterator begin() const { return this->c.begin(); }
     iterator end() { return this->c.end(); }
     const_iterator end() const { return this->c.end(); }
+
+    void pop() {
+      if (this->empty()) {
+        throw std::out_of_range("Stack is empty");
+      }
+      std::stack<T, std::deque<T> >::pop();
+    }
+    T& top() {
+      if (this->empty()) {
+        throw std::out_of_range("Stack is empty");
+      }
+      return std::stack<T, std::deque<T> >::top();
+    }
 };
