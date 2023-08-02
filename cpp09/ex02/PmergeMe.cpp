541f21db64d1f3c1098d7586500e28ded02b026b
Karim Dhrif <karimdhrif4444@gmail.com>
2023-08-02 22:02:24 +0200

    working on insertion and merge sort

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -54,3 +54,67 @@ void printPriorityQueue(std::priority_queue<std::pair<ll, ll>, std::vector<std::
     }
     std::cout << std::endl;
 }
+
+int merge_comp = 0;
+
+void merge(int array[], int const left, int const mid, int const right)
+{
+  int const subArrayOne = mid - left + 1;
+  int const subArrayTwo = right - mid;
+
+  // Create temp arrays
+  int *leftArray = new int[subArrayOne],
+    *rightArray = new int[subArrayTwo];
+
+  // Copy data to temp arrays leftArray[] and rightArray[]
+    for (auto i = 0; i < subArrayOne; i++)
+        leftArray[i] = array[left + i];
+    for (auto j = 0; j < subArrayTwo; j++)
+        rightArray[j] = array[mid + 1 + j];
+
+    std::cout << "SubArrayOne is " << subArrayOne << std::endl;
+    std::cout << "SubArrayTwo is " << subArrayTwo << std::endl;
+    int indexOfSubArrayOne = 0, // Initial index of first sub-array
+        indexOfSubArrayTwo = 0; // Initial index of second sub-array
+    int indexOfMergedArray = left; // Initial index of merged array
+    while (indexOfSubArray one < subArrayOne && indexOfSubArrayTwo < subArrayTwo) {
+        if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo]) {
+            array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+        }
+        else {
+            array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+        }
+        merge_comp++;
+        indexOfMergedArray++;
+    }
+
+    // Copy the remaining elements of left[], if there are any
+    while (indexOfSubArrayOne < subArrayOne) {
+        array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+    // Copy the remaining elements of right[], if there are any
+    while (indexOfSubArrayTwo < subArrayTwo) {
+        array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
+
+    delete[] leftArray;
+    delete[] rightArray;
+}
+
+void mergeSort(int array[], int l, int r) {
+  std::cout << "MergeSort: left is " << l << " and right is " << r << std::endl;
+    if (l < r) {
+        int m = (l + r)/2;
+        std::cout << "MergeSort: mid is " << m << std::endl;
+        mergeSort(array, l, m);
+        mergeSort(array, m + 1, r);
+        merge(array, l, m, r);
+    }
+}
+
