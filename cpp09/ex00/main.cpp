fecf99bc9af8bab7145d4ffff589f053044918f5
Karim DHRIF <kdhrif@made-f0Dr5s2.clusters.42paris.fr>
2023-09-18 16:12:01 +0200

    Fixed segfaults on cpp09ex00

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -1,8 +1,13 @@
 #include "BitcoinExchange.hpp"
 #include <fstream>
+#include <iostream>
 
 int main(int ac, char **argv) {
-	(void)ac;
+	if (ac != 2)
+	{
+		std::cerr << "Usage: ./btc [filename]\n";
+		return 0;
+	}
 	BitcoinExchange exchange("data.csv");
 	exchange.load_csv();
 	exchange.load_psv(argv[1]);
